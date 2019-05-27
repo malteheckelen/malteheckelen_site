27dda2ba4d4925154e56c03faac9e15c1952f736
malteheckelen <malteheckelen@googlemail.com>
2019-05-27 19:30:00 +0200

    global static and template folders

--- malteheckelen_de/settings.py
+++ malteheckelen_de/settings.py
@@ -119,3 +119,36 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+#STATIC_ROOT = '/home/malte/projects/malteheckelen_de/static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/home/malte/projects/malteheckelen_de/static',
+]
+
+#TEMPLATE_DIRS = (
+#        os.path.join(BASE_DIR, "templates"),
+#        '/home/malte/projects/malteheckelen_de/templates',
+#        )
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+#     'django.template.loaders.eggs.Loader',
+)
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, "templates"),
+            '/home/malte/projects/malteheckelen_de/templates',
+],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
